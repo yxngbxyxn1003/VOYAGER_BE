be058291931d633aa85f0778e6a32a6e3b502bd1
Linaeformin <minsa030407@gmail.com>
2025-08-16 23:04:57 +0900

    feat#11_내가 쓴 판매 게시글 목록 반환

--- src/main/java/com/planty/controller/mypage/MypageController.java
+++ src/main/java/com/planty/controller/mypage/MypageController.java
@@ -29,4 +29,16 @@ public class MypageController {
         // 프로필 정보 반환
         return ResponseEntity.ok(mypageService.getProfile(me.getId()));
     }
+
+    // 내가 쓴 판매 게시글 가져오기
+    @GetMapping("/boards")
+    public ResponseEntity<?> getBoards(
+            @AuthenticationPrincipal CustomUserDetails me
+    ) {
+        // 권한이 없을 때
+        if (me == null) return ResponseEntity.status(401).build();
+
+        // 내가 쓴 판매 게시글 반환
+        return ResponseEntity.ok(mypageService.getMySellBoard(me.getId()));
+    }
 }
--- src/main/java/com/planty/service/mypage/MypageService.java
+++ src/main/java/com/planty/service/mypage/MypageService.java
@@ -1,13 +1,17 @@
 package com.planty.service.mypage;
 
 
+import com.planty.dto.mypage.MySellBoardResDto;
 import com.planty.dto.mypage.ProfileResDto;
+import com.planty.entity.board.Board;
 import com.planty.entity.user.User;
+import com.planty.repository.board.BoardRepository;
 import com.planty.repository.mypage.MypageRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 // 마이페이지
@@ -17,9 +21,11 @@ import java.util.Optional;
 public class MypageService {
 
     private final MypageRepository mypageRepository;
+    private final BoardRepository boardRepository;
 
     // 프로필 정보 불러오기
     public ProfileResDto getProfile(Integer userId) {
+        // 유저 확인
         User user = mypageRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없음: " + userId));
 
@@ -29,5 +35,20 @@ public class MypageService {
                 .point(user.getPoint())
                 .profileImg(user.getProfileImg())
                 .build();
+    }
+
+    // 내가 쓴 판매 게시글 불러오기
+    public List<MySellBoardResDto> getMySellBoard(Integer userId) {
+        // 유저 확인
+        User user = mypageRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없음: " + userId));
+
+        // 내가 쓴 판매 게시글 중 판매 중인 게시글
+        List<Board> boards = boardRepository.findMyBoardsOrderByStatusAndCreated(userId);
+
+        // DTO로 변환
+        return boards.stream()
+                .map(MySellBoardResDto::of)
+                .toList();
     }
 }
