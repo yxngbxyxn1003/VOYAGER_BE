b5fb8f6d9bdc9f6b1651624ba334ec184122e703
양보윤 <boyunyang@naver.com>
2025-08-23 01:15:42 +0900

    Update CropRegistrationDto.java

--- src/main/java/com/planty/dto/crop/CropRegistrationDto.java
+++ src/main/java/com/planty/dto/crop/CropRegistrationDto.java
@@ -3,7 +3,6 @@ package com.planty.dto.crop;
 import com.planty.entity.crop.AnalysisStatus;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
@@ -20,5 +19,5 @@ public class CropRegistrationDto {
     private AnalysisStatus analysisStatus;
     private Boolean isRegistered;
     private String cropImg;
-    private MultipartFile imageFile; // 업로드할 이미지 파일
-}
+    // MultipartFile 필드 제거 - JSON 직렬화 문제 해결
+
